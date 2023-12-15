4241705b75b80243e204e922b7f2fe5651a194f0
Uzair Tariq <uzair.t2006@gmail.com>
2023-12-15 20:34:17 +0500

    header and footer completed of jsmastery

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import { CartLink } from '../../CartLink'
+import { useAuth } from '../../../_providers/Auth'
+import Image from 'next/image'
+const HeaderComponent = ({ header }: { header: Header }) => {
+    const pathname = usePathname()
+    const { user } = useAuth()
+
+    return (
+        <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(' ')}>
+            <Gutter className={classes.wrap}>
+                <Link href='/'>
+                    <h2 className={classes.logo}>Logo</h2>
+                </Link>
+
+                <HeaderNav header={header} />
+            </Gutter>
+        </nav>
+    )
+}
+
+export default HeaderComponent
